7edf80d75a72322bd27c5f785158cc817cbbf4b5
JulienM <julienm@marmelab.com>
2024-06-09 13:26:29 +0200

    mobile better ?

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@ defineProps<{
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>Essaie d'attraper le pélican</h3>
+    <h3 id="texte">Essaie d'attraper le pélican</h3>
   </div>
 </template>
 
